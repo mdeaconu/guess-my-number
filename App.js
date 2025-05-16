16778e29f48001f4254a3f543ec1f79f666d3580
Mihai DEACONU <mihai.deaconu@cognizant.com>
2025-05-16 12:54:42 +0300

    Coloring the Components & The Overall App
    Signed-off-by: Mihai DEACONU <mihai.deaconu@cognizant.com>

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import StartGameScreen from "./screens/StartGameScreen";
+import { View } from "react-native";
+import { styles } from "./App.styles";
 
 const App = () => {
-  return <StartGameScreen />;
+  return (
+    <View style={styles.rootScreen}>
+      <StartGameScreen />
+    </View>
+  );
 };
 
 export default App;
