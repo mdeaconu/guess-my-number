fb90f39dd427b2b4ff32cd46c03b65f7820a3aba
Mihai DEACONU <mihai.deaconu@cognizant.com>
2025-05-22 13:51:59 +0300

    Styling the Status Bar
    Signed-off-by: Mihai DEACONU <mihai.deaconu@cognizant.com>

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { LinearGradient } from "expo-linear-gradient";
 import { ImageBackground, SafeAreaView } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { LinearGradient } from "expo-linear-gradient";
 import { useFonts } from "expo-font";
 import AppLoading from "expo-app-loading";
 
@@ -56,19 +57,22 @@ const App = () => {
   }
 
   return (
-    <LinearGradient
-      colors={[Colors.primary700, Colors.accent500]}
-      style={styles.rootScreen}
-    >
-      <ImageBackground
-        source={require("./assets/images/background.png")}
-        resizeMode="cover"
+    <>
+      <StatusBar style="light" />
+      <LinearGradient
+        colors={[Colors.primary700, Colors.accent500]}
         style={styles.rootScreen}
-        imageStyle={styles.backgroundImage}
       >
-        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
-      </ImageBackground>
-    </LinearGradient>
+        <ImageBackground
+          source={require("./assets/images/background.png")}
+          resizeMode="cover"
+          style={styles.rootScreen}
+          imageStyle={styles.backgroundImage}
+        >
+          <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
+        </ImageBackground>
+      </LinearGradient>
+    </>
   );
 };
 
